e1fb3cf3a90d1aefd0606a990779cc577c0b2280
Karishma pawar <kspatil1693@gmail.com>
2022-06-28 22:25:27 -0700

    Added first project

---
+++ src/main/java/LoginServlet/loginServlet.java
@@ -0,0 +1,62 @@
+package LoginServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+/**
+ * Servlet implementation class loginServlet
+ */
+@WebServlet("/loginServlet")
+public class loginServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public loginServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	
+		String emailId=request.getParameter("emailId");
+		String password=request.getParameter("password");
+		
+		PrintWriter out=response.getWriter();
+	    out.println("dashboardServlet?emailId="+emailId+"password"+password);
+		//out.println("dashboardServlet?password="+password);
+		
+      		
+		HttpSession session = request.getSession();
+		session.setAttribute("emailId", emailId);
+		session.setAttribute("password", password);
+		
+		
+		response.sendRedirect("dashboardServlet");
+		
+		 }
+	
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+		
+		
+		
+	}
+
+}
